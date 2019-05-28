0b6a53a55ef50be0e9272778a264f65c5d85133c
Rolanda Powell <rolanda_show@Rolandas-MacBook-Pro.local>
2019-05-27 21:29:36 -0400

    created game logic

--- memory_game/main.js
+++ memory_game/main.js
@@ -1,6 +1,15 @@
 console.log("Up and running!");
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
 console.log("User flipped " + cardOne);
+console.log("User flipped " + cardThree);
+var cards = ['queen' , 'queen' , 'king' , 'king'];
+var cardsInPlay [];
+var cardOne = cards[0];
+cardsInPlay.push('cardOne');
+var cardTwo [2];
+cardsInPlay.push('cardTwo');
+console.log("User flipped " + cardOne);
+console.log ("User flipped " + cardTwo);
+if (cardsInPlay ==== 2);
+
+	alert("You found a match!");
+
